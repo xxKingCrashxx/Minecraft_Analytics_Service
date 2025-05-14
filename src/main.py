0e3a8eecc5f7ac5e498a447fd56b6ec31668bd69
xxking_crashxx <ajdecker2@gmail.com>
2025-05-14 18:16:52 -0400

    added route, updated gitignore

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from pathlib import Path
+from src.routes.players import player_router
 
 load_dotenv(Path(__file__).resolve().parent.parent / ".env")
+
 app = FastAPI()
+
+
+app.include_router(player_router)
 
 @app.get("/")
 async def root():
